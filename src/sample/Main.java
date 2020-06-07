341af92517b7cadc1ef13294bcc2221b578c0f30
LUNKUS <sigitas@lunkus.com>
2020-06-07 08:11:40 +0300

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Orientation;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        primaryStage.setTitle("Life Cycle");
+
+        FlowPane root = new FlowPane(Orientation.VERTICAL, 5 ,5);
+
+        Scene scene = new Scene(root, 700, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
